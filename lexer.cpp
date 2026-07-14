a6b477ca13f464f9475021855af40cd67db77038
owaismughal129-droid <owaismughal129@gmail.com>
2026-07-14 12:06:26 +0500

    Commit 9: Add logic to identify assignment and math operators

--- lexer.cpp
+++ lexer.cpp
@@ -1,19 +1,24 @@
 #include "lexer.h"
 #include <cctype>
 
-// Commit 5: Empty function
 std::vector<Token> Lexer::tokenize(std::string input) {
     std::vector<Token> tokens;
     for (size_t i = 0; i < input.length(); i++) {
-         if (isspace(input[i])) continue; // Skip spaces
+        if (isspace(input[i])) continue; // Skip spaces
 
-          if (isalpha(input[i])) { // It's an identifier/variable
+        if (isalpha(input[i])) { // It's an identifier/variable
             std::string id = "";
             while (i < input.length() && isalpha(input[i])) {
                 id += input[i++];
             }
             tokens.push_back({"ID", id});
             i--; 
+        } 
+        else if (input[i] == '=') {
+            tokens.push_back({"ASSIGN", "="});
+        }
+        else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/') {
+            tokens.push_back({"OP", std::string(1, input[i])});
         }
     }
     return tokens;
