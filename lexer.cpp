d8ddd3464b491eb3ddfdfb91b2aee5d5790e876b
owaismughal129-droid <owaismughal129@gmail.com>
2026-07-14 12:04:45 +0500

    Commit 6: Add main for loop to read string character by character

--- lexer.cpp
+++ lexer.cpp
@@ -4,5 +4,7 @@
 // Commit 5: Empty function
 std::vector<Token> Lexer::tokenize(std::string input) {
     std::vector<Token> tokens;
+    for (size_t i = 0; i < input.length(); i++) {
+    }
     return tokens;
 }
