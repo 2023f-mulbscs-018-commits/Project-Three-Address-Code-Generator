8746f3deddcc491e07facf45d8678b6017007a5d
owaismughal129-droid <owaismughal129@gmail.com>
2026-07-14 12:06:01 +0500

    Commit 8: Add logic to extract variable names (Identifiers)

--- lexer.cpp
+++ lexer.cpp
@@ -7,6 +7,14 @@ std::vector<Token> Lexer::tokenize(std::string input) {
     for (size_t i = 0; i < input.length(); i++) {
          if (isspace(input[i])) continue; // Skip spaces
 
+          if (isalpha(input[i])) { // It's an identifier/variable
+            std::string id = "";
+            while (i < input.length() && isalpha(input[i])) {
+                id += input[i++];
+            }
+            tokens.push_back({"ID", id});
+            i--; 
+        }
     }
     return tokens;
 }
