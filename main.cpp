ed211869a4e6e52ebe67c3a9570953b5f48c0878
2023f-mulbscs-018-commits <2023f-mulbscs-018@mul.edu.pk>
2026-07-15 09:09:43 +0500

    Commit 6: Instantiate Lexer and generate token vector

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
 #include<iostream>
 #include<string>
+#include "lexer.h"
+#include "parser.h"
+#include "tac.h"
 using namespace std;
+
+int main() {
+    cout << "--- Starting TAC Compiler Pipeline ---\n" << endl;
+
+    string sourceCode = "x = a + b"; // The code to compile
+    cout << "Input Source: " << sourceCode << "\n" << endl;
+
+    cout << "\n--- Compilation Complete ---" << endl;
+    // Step 1: Lexical Analysis
+    Lexer lexer;
+    vector tokens = lexer.tokenize(sourceCode);
+    return 0;
+
+}
