514557afccdb059b07e2804ee380281deef5b5e9
ahmiijut <juttsaab5715@gmail.com>
2026-07-15 11:21:18 +0500

    Commit 2: Define the ASTNode struct in parser.h

--- parser.h
+++ parser.h
@@ -5,4 +5,13 @@
 #include <string>
 #include <vector>
 
+// A node in the syntax tree
+struct ASTNode {
+    std::string value;
+    ASTNode* left;
+    ASTNode* right;
+    
+    ASTNode(std::string val) : value(val), left(nullptr), right(nullptr) {}
+};
+
 #endif
